b718747fd95d6eae745b1430d9a3f5ab56fc29bc
Mike Lueders <mike@roadkill.org>
2018-02-03 10:48:44 -0600

    ./gradlew addPostgresContainer

--- src/componentTest/groovy/org/dreamscale/sample/ComponentTest.java
+++ src/componentTest/groovy/org/dreamscale/sample/ComponentTest.java
@@ -10,6 +10,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@PersistenceTest
 @ActiveProfiles({"local", "test", "componentTest"})
 @SpringBootTest(
         classes = {ComponentTestConfig.class},
